35d14217942258f500c68c1252e1fdaf5f2e39c9
Vladimir Logachev <logachev.dev@ya.ru>
2021-08-30 00:03:29 +0700

    Check if it builds per se

--- backend/1.init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS events (
-    "event_id" serial NOT NULL PRIMARY KEY,
-    "ts" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "uuid" uuid NOT NULL,
-    "body" jsonb NOT NULL,
-    CONSTRAINT unique_event_uuid UNIQUE ("uuid")
-);
